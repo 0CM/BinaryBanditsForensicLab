0f2536a1ff79c468f7e7a36b7e9d79de1b1c2c65
0CM <jiri.holoska@vse.cz>
2024-07-27 18:13:36 +0200

    GetTools Script Updated

--- packages/BanditLabGetTools.sh
+++ packages/BanditLabGetTools.sh
@@ -1,45 +1,51 @@
 #!/bin/bash
 
+#Create Folder Structure
+sudo -u ubuntu sh -c 'mkdir "$HOME"/DATA'
+sudo -u ubuntu sh -c 'mkdir $HOME/ewfmount'
+sudo -u ubuntu sh -c 'mkdir $HOME/evidence'
+sudo -u ubuntu sh -c 'mkdir $HOME/tools'
+sudo -u ubuntu sh -c 'mkdir $HOME/tools/hayabusa'
+sudo -u ubuntu sh -c 'mkdir $HOME/tools/memprocfs'
+sudo -u ubuntu sh -c 'mkdir $HOME/tools/trufflehog'
+sudo -u ubuntu sh -c 'mkdir $HOME/tools/noseyparker'
+sudo -u ubuntu sh -c 'mkdir $HOME/tools/pup'
+sudo -u ubuntu sh -c 'mkdir $HOME/tools/vt'
+sudo -u ubuntu sh -c 'mkdir $HOME/tools/nuclei'
+sudo -u ubuntu sh -c 'mkdir $HOME/tools/chainsaw'
 # Get the machine architecture
-ARCH=$(uname -m)
 
-#Arch independent packages and tools
-```
-sudo -u ubuntu python3 -m venv /home/ubuntu/pyapps
-sudo -u ubuntu python3 -m venv /home/ubuntu/pyflare
-sudo -u ubuntu chmod 755 /home/ubuntu/pyapps/bin/hindsight.py
-sudo -u ubuntu chmod 755 /home/ubuntu/pyapps/bin/hindsight_gui.py
-sudo -u ubuntu /home/ubuntu/pyflare/bin/python3 -m pip install flare-capa
-sudo -u ubuntu /home/ubuntu/pyapps/bin/python3 -m pip install peepdf-3 pdfid oletools pyhindsight browserexport windowsprefetch xlsxgrep tabulate dnspython wheel
-sudo -u ubuntu /home/ubuntu/pyapps/bin/python3 -m pip install https://github.com/msuhanov/dfir_ntfs/archive/1.1.18.tar.gz
-sudo -u ubuntu /home/ubuntu/pyapps/bin/python3 -m pip install git+https://github.com/cisagov/ioc-scanner.git
-sudo -u ubuntu sh -c 'git clone https://github.com/CISOfy/lynis $HOME/tools/lynis'
-```
+ARCH=$(uname -m) 
 # Check the architecture and execute corresponding commands
-if [ "$ARCH" = "aarch64" ]; then
-    sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/EZTools_aarch64-20240710.tar.gz| tar -xz -C $HOME/tools/'
+echo "$ARCH packages will be installed .."
+if [ "$ARCH = "aarch64" ]; then
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/EZTools_aarch64-20240710.tar.gz| tar -xz -C $HOME/tools/'
 	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/sidr_aarch64-20240702.tar.gz| tar -xz -C $HOME/tools/'
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/ufrisk/MemProcFS/releases/download/v5.9/MemProcFS_files_and_binaries_v5.9.18-linux_aarch64-20240613.tar.gz| tar -xz -C $HOME/tools/ --one-top-level=memprocfs' 
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/Yamato-Security/hayabusa/releases/download/v2.16.0/hayabusa-2.16.0-linux-arm.zip |  bsdtar -xvf- -C $HOME/tools/hayabusa'
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/trufflesecurity/trufflehog/releases/download/v3.79.0/trufflehog_3.79.0_linux_arm64.tar.gz| tar -xz -C $HOME/tools/trufflehog --one-top-level=trufflehog'
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/praetorian-inc/noseyparker/releases/download/v0.18.1/noseyparker-v0.18.1-aarch64-unknown-linux-gnu.tar.gz | tar -xz -C $HOME/tools/ --one-top-level=noseyparker'
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/chainsaw_aarch64-20240713.tar.gz| tar -xz -C $HOME/tools/ --one-top-level=chainsaw'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/ufrisk/MemProcFS/releases/download/v5.10/MemProcFS_files_and_binaries_v5.10.1-linux_aarch64-20240721.tar.gz| tar -xvz -C $HOME/tools/memprocfs' 
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/Yamato-Security/hayabusa/releases/download/v2.16.0/hayabusa-2.16.0-linux-arm.zip | bsdtar -xvf- -C $HOME/tools/hayabusa'
+	sudo -u ubuntu chmod 755 $HOME/tools/hayabusa/hayabusa-2.16.0-lin-aarch64-gnu
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/trufflesecurity/trufflehog/releases/download/v3.79.0/trufflehog_3.79.0_linux_arm64.tar.gz| tar -xz -C $HOME/tools/trufflehog'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/praetorian-inc/noseyparker/releases/download/v0.18.1/noseyparker-v0.18.1-aarch64-unknown-linux-gnu.tar.gz | tar -xz -C $HOME/tools/noseyparker'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/chainsaw_aarch64-20240713.tar.gz| tar -xz -C $HOME/tools/chainsaw'
 	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/timeliner_aarch64-20240717.tar.gz| tar -xz -C $HOME/tools/'
-	sudo -u ubuntu sh -c 'git clone https://github.com/SigmaHQ/sigma $HOME/tools/chainsaw/sigma'
-	sudo -u ubuntu chmod 755 /home/ubuntu/tools/hayabusa/hayabusa-2.16.0-lin-aarch64-gnu
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO-  https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_arm64.zip |  bsdtar -xvf- -C $HOME/tools/pup && sudo ln -s $HOME/tools/pup/pup /usr/bin/pup && chmod 755 $HOME/tools/pup/pup'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO-  https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_arm64.zip |  bsdtar -xvf- -C $HOME/tools/pup/ && sudo ln -s $HOME/tools/pup/pup /usr/bin/pup && chmod 755 $HOME/tools/pup/pup'
 	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/vt-cli_aarch64-20240720.tar.gz| tar -xz -C $HOME/tools/vt && sudo ln -s $HOME/tools/vt/vt /usr/bin/vt'
 	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/projectdiscovery/nuclei/releases/download/v3.3.0/nuclei_3.3.0_linux_arm64.zip |  bsdtar -xvf- -C $HOME/tools/nuclei && sudo ln -s $HOME/tools/nuclei/nuclei /usr/bin/nuclei && chmod 755 $HOME/tools/nuclei/nuclei'
-elif [ "$ARCH" = "x86_64" ]; then
+elif [ $ARCH = "x86_64" ]; then
     echo "Running commands for x86_64 architecture"
-    sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/EZTools_aarch64-20240710.tar.gz| tar -xz -C $HOME/tools/'
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/sidr_aarch64-20240702.tar.gz| tar -xz -C $HOME/tools/'
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/ufrisk/MemProcFS/releases/download/v5.9/MemProcFS_files_and_binaries_v5.9.18-linux_aarch64-20240613.tar.gz| tar -xz -C $HOME/tools/ --one-top-level=memprocfs'
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/Yamato-Security/hayabusa/releases/download/v2.16.0/hayabusa-2.16.0-linux-arm.zip |  bsdtar -xvf- -C $HOME/tools/hayabusa'
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/trufflesecurity/trufflehog/releases/download/v3.79.0/trufflehog_3.79.0_linux_arm64.tar.gz| tar -xz -C $HOME/tools/trufflehog --one-top-level=trufflehog'
-	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/praetorian-inc/noseyparker/releases/download/v0.18.1/noseyparker-v0.18.1-aarch64-unknown-linux-gnu.tar.gz | tar -xz -C $HOME/tools/ --one-top-level=noseyparker'
-	sudo -u ubuntu sh -c 'git clone https://github.com/SigmaHQ/sigma $HOME/tools/chainsaw/sigma'
-	sudo -u ubuntu sh -c 'git clone https://github.com/CISOfy/lynis $HOME/tools/lynis'
+    sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/EZTools_x86_64-20240725.tar.gz| tar -xz -C $HOME/tools/'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/sidr_x86_64-20240725.tar.gz| tar -xz -C $HOME/tools/'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/ufrisk/MemProcFS/releases/download/v5.10/MemProcFS_files_and_binaries_v5.10.1-linux_x64-20240721.tar.gz| tar -xz -C $HOME/tools/ --one-top-level=memprocfs'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/Yamato-Security/hayabusa/releases/download/v2.16.0/hayabusa-2.16.0-linux-intel.zip | bsdtar -xvf- -C $HOME/tools/hayabusa'
+	sudo -u ubuntu chmod 755 $HOME/tools/hayabusa/hayabusa-2.16.0-lin-x64-musl
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/trufflesecurity/trufflehog/releases/download/v3.80.1/trufflehog_3.80.1_linux_amd64.tar.gz| tar -xz -C $HOME/tools/trufflehog --one-top-level=trufflehog'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/praetorian-inc/noseyparker/releases/download/v0.18.1/noseyparker-v0.18.1-x86_64-unknown-linux-gnu.tar.gz | tar -xz -C $HOME/tools/ --one-top-level=noseyparker'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/WithSecureLabs/chainsaw/releases/download/v2.9.2/chainsaw_x86_64-unknown-linux-gnu.tar.gz| tar -xz -C $HOME/tools/ --one-top-level=chainsaw'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/timeliner_x86_64-20240725.tar.gz| tar -xz -C $HOME/tools/'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/ericchiang/pup/releases/download/v0.4.0/pup_v0.4.0_linux_amd64.zip |  bsdtar -xvf- -C $HOME/tools/pup && sudo ln -s $HOME/tools/pup/pup /usr/bin/pup && chmod 755 $HOME/tools/pup/pup'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/VirusTotal/vt-cli/releases/download/1.0.0/Linux64.zip|  bsdtar -xvf- -C $HOME/tools/vt && sudo ln -s $HOME/tools/vt/vt /usr/bin/vt'
+	sudo -u ubuntu sh -c 'wget --no-check-certificate -qO- https://github.com/projectdiscovery/nuclei/releases/download/v3.3.0/nuclei_3.3.0_linux_amd64.zip |  bsdtar -xvf- -C $HOME/tools/nuclei && sudo ln -s $HOME/tools/nuclei/nuclei /usr/bin/nuclei && chmod 755 $HOME/tools/nuclei/nuclei'
+
 else
     echo "Unsupported architecture: $ARCH"
 fi
