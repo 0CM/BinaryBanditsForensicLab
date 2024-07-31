b30bc847a9acbd33e5d99c421dd766a8fa863a63
0CM <jiri.holoska@vse.cz>
2024-07-31 17:29:53 +0200

    DL script for x86 binaries to 3rd party linux distros

--- packages/BanditLabTools_x86_64-PortedOnly.sh
+++ packages/BanditLabTools_x86_64-PortedOnly.sh
@@ -1,5 +1,5 @@
 # Create Folder Structure
-sudo sh -c "mkdir -p $HOME/BanditLabTools"
-sudo sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/EZTools_x86_64-20240725.tar.gz | tar -xz -C "$HOME"/tools/'
-sudo sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/sidr_x86_64-20240725.tar.gz | tar -xz -C "$HOME"/tools/'
-sudo sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/timeliner_x86_64-20240725.tar.gz | tar -xz -C "$HOME"/tools/'
+sh -c "mkdir -p $HOME/BanditLabTools"
+sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/EZTools_x86_64-20240725.tar.gz | tar -xz -C "$HOME"/BanditLabTools/'
+sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/sidr_x86_64-20240725.tar.gz | tar -xz -C "$HOME"/BanditLabTools/'
+sh -c 'wget --no-check-certificate -qO- https://github.com/0CM/BinaryBanditsForensicLab/raw/main/packages/timeliner_x86_64-20240725.tar.gz | tar -xz -C "$HOME"/BanditLabTools/'
